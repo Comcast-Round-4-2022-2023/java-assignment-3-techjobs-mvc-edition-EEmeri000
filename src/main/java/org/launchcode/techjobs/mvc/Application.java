49401f9d5cf8cd168e806d223690a94d492607e7
CannibalCrab <138933689+EEmeri000@users.noreply.github.com>
2023-09-18 14:20:56 -0400

    Initial Commit

--- src/main/java/org/launchcode/techjobs/mvc/Application.java
+++ src/main/java/org/launchcode/techjobs/mvc/Application.java
@@ -2,7 +2,7 @@ package org.launchcode.techjobs.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Initial commit comment
 @SpringBootApplication
 public class Application {
 
